3315b266b1baa5eb23e18e88eb3672922dc3a802
Albin Persson <XALPE@ttsgroup.com>
2019-04-17 15:09:02 +0200

    Initial commit

---
+++ Threadtest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Threadtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(WrY);
+            t.Start();
+
+            for (int i = 0; i < 1000; i++) Console.Write("x");
+
+            void WrY()
+            {
+                for (int i = 0; i < 1000; i++) Console.Write("y");
+            }
+        }
+
+        
+    }
+}
