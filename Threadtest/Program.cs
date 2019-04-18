51158dcfddd47c9de54499e6c65c87fa64049320
Albin Persson <23519801+DenmarkPolice@users.noreply.github.com>
2019-04-18 11:12:35 +0200

    Sleep

--- Threadtest/Program.cs
+++ Threadtest/Program.cs
@@ -7,11 +7,19 @@ namespace Threadtest
     {
         static void Main(string[] args)
         {
-            Thread t = new Thread(WrY);
+            Thread t = new Thread(sleepyThread);
             t.Start();
+
 
             for (int i = 0; i < 1000; i++) Console.Write("x");
 
+            void sleepyThread()
+            {
+                Thread.Sleep(5000);
+                WrY();
+                Thread.Sleep(5000);
+                WrY();
+            }
             void WrY()
             {
                 for (int i = 0; i < 1000; i++) Console.Write("y");
